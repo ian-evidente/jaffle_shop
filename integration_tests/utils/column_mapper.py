4b7854406ec9fabd77a0672da5fdaea0352c1e08
Ian Evidente <evidente.ian@gmail.com>
2024-03-15 16:38:16 -0500

    add get_model_compiled_code method

--- integration_tests/utils/column_mapper.py
+++ integration_tests/utils/column_mapper.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import pandas as pd
 
+
 class DbtColumnMapper:
     def __init__(self, artifact_path: str = None):
         self.artifact_path = artifact_path or '../target'
@@ -49,20 +50,40 @@ class DbtColumnMapper:
 
         return pd.DataFrame(dependencies)
 
+    def get_model_compiled_code(self, model: str) -> str:
+        # Read manifest.json file
+        with open(self.manifest_path, 'r') as f:
+            manifest_data = json.load(f)
+
+        compiled_code = ""
+        # Iterate over nodes in the manifest
+        for node_name, node_data in manifest_data.get('nodes', {}).items():
+            # Check if the node matches the specified model
+            if node_name.endswith(model):
+                # Retrieve the compiled code for the specified model
+                compiled_code = node_data.get('compiled_code', '')
+                break
+
+        return compiled_code
+
 
 def main():
-    parser = argparse.ArgumentParser(description="DBT Column Mapper")
-    parser.add_argument("-s", "--model", type=str, help="Specify the model name")
+    parser = argparse.ArgumentParser(description="dbt Column Mapper")
+    parser.add_argument("-s", "--select", type=str, help="Specify the model name")
     args = parser.parse_args()
+    model = args.select
 
-    if args.model:
+    if model:
         dbt_mapper = DbtColumnMapper()
-        columns_df = dbt_mapper.get_columns(args.model)
-        depends_on_df = dbt_mapper.get_model_depends_on(args.model)
+        columns_df = dbt_mapper.get_columns(model)
+        depends_on_df = dbt_mapper.get_model_depends_on(model)
+        compiled_code = dbt_mapper.get_model_compiled_code(model)
         print("Columns:")
         print(columns_df)
         print("\nDependencies:")
         print(depends_on_df)
+        print("\nCompiled Code:")
+        print(compiled_code)
     else:
         print("Please specify the model name using the -s/--model option.")
 
