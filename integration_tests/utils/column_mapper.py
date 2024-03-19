6ba96c874855441b1ceab342603b6edbac6955a6
Ian Evidente <evidente.ian@gmail.com>
2024-03-19 11:31:47 -0500

    remove comments; update .gitignore

--- integration_tests/utils/column_mapper.py
+++ integration_tests/utils/column_mapper.py
@@ -14,56 +14,41 @@ class DbtColumnMapper:
     def get_columns(self, datasets: list[str]) -> pd.DataFrame:
         columns_data = []
 
-        # Read the catalog.json file
         with open(self.catalog_path, 'r') as f:
             catalog_data = json.load(f)
 
-        # Iterate through nodes for each dataset
         for dataset in datasets:
-            # Iterate through nodes to find the specified model
             for node_name, node_data in catalog_data.get('nodes', {}).items():
                 if node_name.endswith(dataset):
-                    # Extract columns for the found model
                     for col_name in node_data.get('columns', {}).keys():
                         columns_data.append({'model_name': dataset, 'column_name': col_name})
-                    break  # Stop iteration once the model is found
+                    break
 
         return pd.DataFrame(columns_data)
 
     def get_model_dependencies(self, model: str) -> pd.DataFrame:
         dependencies = []
 
-        # Read manifest.json file
         with open(self.manifest_path, 'r') as f:
             manifest_data = json.load(f)
 
-        # Iterate over nodes in the manifest
         for node_name, node_data in manifest_data.get('nodes', {}).items():
-            # Check if the node matches the specified model
             if node_name.endswith(model):
-                # Get dependencies for the specified model
                 model_dependencies = node_data.get('depends_on', {})
-                # Iterate over the nodes in dependencies
                 for node in model_dependencies.get('nodes', []):
-                    # Extract the node type from the node name
                     node_type = node.split('.')[0]
-                    dependencies.append(
-                        {'model_name': model, 'depends_on': node.split('.')[-1], 'node_type': node_type})
-                break  # Stop searching once the model is found
+                    dependencies.append({'model_name': model, 'depends_on': node.split('.')[-1], 'node_type': node_type})
+                break
 
         return pd.DataFrame(dependencies)
 
     def get_compiled_code(self, model: str) -> str:
-        # Read manifest.json file
         with open(self.manifest_path, 'r') as f:
             manifest_data = json.load(f)
 
         compiled_code = ""
-        # Iterate over nodes in the manifest
         for node_name, node_data in manifest_data.get('nodes', {}).items():
-            # Check if the node matches the specified model
             if node_name.endswith(model):
-                # Retrieve the compiled code for the specified model
                 compiled_code = node_data.get('compiled_code', '')
                 break
 
@@ -71,29 +56,21 @@ class DbtColumnMapper:
 
     @staticmethod
     def replace_final_select_columns(sql_query: str, columns: list) -> str:
-        # Find the last SELECT statement in the SQL query
         last_select_match = re.finditer(r'select\s+(?:(?!\bselect\b).)*$', sql_query, re.IGNORECASE | re.MULTILINE)
         last_select_indices = [match.span() for match in last_select_match]
         if last_select_indices:
             last_select_start, last_select_end = last_select_indices[-1]
             last_select_statement = sql_query[last_select_start:last_select_end]
-            # Replace the * with the provided list of columns
             modified_last_select = re.sub(r'\*', ', '.join(columns), last_select_statement)
-            # Replace the original last SELECT statement with the modified one in the SQL query
             modified_sql_query = sql_query[:last_select_start] + modified_last_select + sql_query[last_select_end:]
             return modified_sql_query
         else:
-            # If no SELECT statement is found, return the original SQL query
             return sql_query
 
     @staticmethod
     def replace_cte_select_columns(cte_query: str, cte_table: str, columns: list) -> str:
-        # Define the regular expression pattern to match the SELECT statement in the CTE
         pattern = rf'(\b[a-z_]*\b\s+as\s+\(\s*select\s+)(\*)\s+(from\s+{cte_table}\s*\))'
-
-        # Replace the '*' with the provided list of columns
         modified_query = re.sub(pattern, rf'\1{", ".join(columns)} \3', cte_query, flags=re.IGNORECASE)
-
         return modified_query
 
     def reformat_compiled_code(self, model: str) -> str:
@@ -111,8 +88,6 @@ class DbtColumnMapper:
             reformatted = self.replace_cte_select_columns(cte_query=reformatted, cte_table=d, columns=d_columns)
 
         return reformatted
-
-    # def analyze_compiled_code(self, compiled_code: str, depends_on: list[str]):
 
 
 def main():
